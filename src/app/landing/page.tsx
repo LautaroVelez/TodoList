c389cc6725c5e2c05d3eb4a97a28eded47d61399
LautaroVelez <lautivele28@gmail.com>
2024-08-26 00:28:27 -0300

    landing added

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,7 @@
+export default function Landing(){
+    return(
+        <div>
+            <h1 className="text-center text-3xl text-white font-bold mt-10">TO-DO LIST LAUTI TYPESCRIPT PRIMER PROYECTO</h1>
+        </div>
+    )
+}
