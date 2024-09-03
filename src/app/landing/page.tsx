bd5c3a59bbfe6113c9a31896a32da298c7560815
LautaroVelez <lautivelez28@gmail.com>
2024-09-02 21:37:25 -0300

    ALL WORKING HEHE

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -2,15 +2,32 @@
 'use client'
 
 import React, {useEffect, useState} from "react";
-import {Input, Button, Divider, Card, CardHeader, CardBody, Checkbox, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from '@nextui-org/react';
+import {
+    Input,
+    Button,
+    Divider,
+    Card,
+    CardHeader,
+    CardBody,
+    Checkbox, ButtonGroup
+} from '@nextui-org/react';
 import {IoMdAdd} from "react-icons/io";
+import Image from 'next/image'
+import illustration from '../../../public/illustration.png'
+import plant from '../../../public/plant.png'
+import {FaRegTrashAlt} from "react-icons/fa";
 
 
 export default function Landing() {
     const [descTask, setDescTask] = useState<string>('')
     const [ListOfTasks, setListOfTasks] = useState<Array<Task>>([]);
+    const [filterList, setFilterList] = useState<Array<Task>>([]);
     const [type, setType] = useState<"uncompleted" | "completed">("uncompleted");
-    const [countCompleted, setCountCompleted] = useState<number>(0)
+
+    const [selectedType, setSelectedType] = useState<string>()
+    const filters = ["all", "completed", "uncompleted"]
+    const [backgroundColor, setBackgroundColor] = useState("bg-[#e6b78f]");
+
 
     interface Task {
         des: string;
@@ -22,7 +39,7 @@ export default function Landing() {
     function createTask(): Task {
         return {
             des: descTask,
-            id: ListOfTasks.length,
+            id: ListOfTasks.length + 1,
             type: "uncompleted"
         };
     }
@@ -31,64 +48,126 @@ export default function Landing() {
     function HandleAddTask(e: React.FormEvent) {
         e.preventDefault()
         const task = createTask()
-        setListOfTasks([...ListOfTasks, task]);
+        setListOfTasks([...ListOfTasks, task])
+        setDescTask('');
     }
+
 
     function handleType(id: number) {
-        setCountCompleted(+1)
-        setListOfTasks(ListOfTasks.map(task => task.id === id ? {...task, type: "completed"} : task))
+        setListOfTasks(ListOfTasks.map(task => {
+            if (task.id === id) {
+                const newType = task.type === "completed" ? "uncompleted" : "completed";
+
+                return {...task, type: newType};
+            }
+            return task;
+        }));
     }
 
+
+    function hanldeDelete(id: number) {
+        setListOfTasks(ListOfTasks.filter((task) => task.id !== id))
+    }
+
+    function handleFilterButtonClick(type) {
+        switch (type) {
+            case "completed":
+                setFilterList(ListOfTasks.filter(task => task.type === "completed"))
+                setSelectedType(type)
+                break;
+            case "uncompleted":
+                setFilterList(ListOfTasks.filter(task => task.type === "uncompleted"))
+                setSelectedType(type)
+                break;
+            case "all":
+                setFilterList(ListOfTasks)
+                setSelectedType(type)
+                break;
+        }
+    }
+
+    useEffect(() => {
+        setFilterList(ListOfTasks)
+    }, [ListOfTasks]);
+
     return (
-        <div className="bg-gradient-to-r from-purple-500 to-purple-900 h-full min-h-screen items-center ">
-            <ul className="text-white">
-                <li>eliminar tareas: listo</li>
-                <li>marcar como completada: listo</li>
-                <li>filtrar tareas incompletas, ocmpletas, todas</li>
-                <li>las tareas deben durar por mas que se recarge la pagina</li>
-                <li>optimizar el rendimiento ver usecallback o react.memo</li>
-            </ul>
-            <div className='w-full justify-center flex items-center h-full'>
-                <Card isBlurred className="border-none bg-background/60 bg-100/50 w-[600px] p-8 pb-52" shadow="sm">
-                    <CardHeader>
+        <>
+            <div
+                className="bg-gradient-to-tr from-orange-400 to-orange-300 h-full min-h-screen flex items-center justify-center relative">
+                <div className='w-full justify-center flex items-center h-full'>
+
+                    <Card
+                        className="border-none bg-background/60 bg-100/50 w-[600px] md:h-[600px] h-[700px] p-8 pb-52"
+                        shadow="md">
+
                         <div className="w-full">
-                            <h1 className="text-white font-bold text-3xl">My tasks</h1>
-                            <Divider orientation='horizontal' className="bg-white"/>
-                        </div>
-                    </CardHeader>
+                            <h1 className="text-white drop-shadow-2xl text-center underline text-8xl tasksOracion">My Tasks</h1>
+                            <div className={'flex w-full justify-center my-3'}>
 
-                    <CardBody>
-                        <form onSubmit={HandleAddTask}>
-                            <div className="flex w-full justify-between">
-                                <Input placeholder="Add a task" value={descTask} className="w-[70%]" required
-                                       onChange={(e) => setDescTask(e.target.value)}/>
-                                <Button className="w-[150px] bg-gradient-to-r from-slate-600 to-gray-800" type="submit"
-                                        isIconOnly color="secondary" endContent={<IoMdAdd/>}></Button>
+
+                                {filters.map((type, index) => (
+                                    <button
+                                        onClick={() => handleFilterButtonClick(type)}
+                                        className={`button first-letter:uppercase ${selectedType === type? "active" : ""}`}
+                                        key={index}>{type}</button>
+                                ))}
+
+
                             </div>
-                        </form>
-
-
-                        {ListOfTasks.map((task, index) => (
-
-                            <>
-                                <div key={index}
-                                     className="flex mt-3 justify-between outline outline-1 items-center p-3 h-full rounded-2xl outline-white text-white">
-                                    <div className="flex h-full w-full items-center pl-2 text-center">
-                                        <Checkbox color="secondary" onChange={() => {
-                                            handleType(task.id)
-                                        }}
-                                                  lineThrough
-                                                  classNames={{label: "w-full text-white pl-2"}}>{task.des}{task.id}</Checkbox>
-                                        <h1>{task.type}</h1>
-                                    </div>
+                            <form onSubmit={HandleAddTask}>
+                                <div className="flex w-full justify-between items-center">
+                                    <Input placeholder="Add a task" value={descTask} color={'default'}
+                                           radius={'sm'}
+                                           className="w-[70%] font-bold first-letter:uppercase" required
+                                           onChange={(e) => setDescTask(e.target.value)}/>
+                                    <Button className="buttonAdd w-[150px] bg-[#aecd68]"
+                                            type="submit"
+                                            isIconOnly color="secondary"
+                                            endContent={<IoMdAdd className={'size-7 text-black'}/>}></Button>
                                 </div>
-                            </>
-                        ))}
-                    </CardBody>
-                </Card>
+                            </form>
+                        </div>
 
+
+                        <div className="h-[320px] mt-2">
+                            <div className="relative h-full overflow-y-auto">
+                                {filterList.map((task, index) => {
+
+                                    return (
+                                        <div key={task.id}
+                                             className={` ${task.type === "completed" ? "bg-[#E6B78F61]" : "bg-[#e6b78f]"} p-4 flex justify-between rounded-xl mt-3`}>
+                                            <Checkbox color="success" onChange={() => {
+                                                handleType(task.id)
+
+                                            }}
+                                                        isSelected={task.type === "completed"}
+                                                      lineThrough
+                                                      classNames={{label: "w-full text-[#71432D]  font-bold pl-2"}}>
+                                                <p
+                                                    className={'first-letter:uppercase'}>{task.des}</p>
+                                            </Checkbox>
+                                            <span><button
+                                                onClick={() => hanldeDelete(task.id)}><FaRegTrashAlt
+                                                className={'text-[#e8e8e8] hover:text-red-600 transition'}/></button></span>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </Card>
+
+                    <div className="absolute bottom-0 right-0">
+                        <Image src={illustration} alt={'illustration'} width={300} height={200} priority
+                               className="object-cover md:w-[15vw] w-[20vw]"/>
+                    </div>
+                    <div className="absolute bottom-0 left-0">
+                        <Image src={plant} alt={'plant'} width={200} height={200} priority
+                               className="object-cover md:w-[10vw] w-[20vw]"/>
+                    </div>
+
+                </div>
 
             </div>
-        </div>
+        </>
     )
 }
